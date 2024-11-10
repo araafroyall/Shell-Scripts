84fd39fcbe21ce630202ae210a836ff265b5206b
AraafRoyall ❂ <swordigoar@gmail.com>
2024-11-10 12:21:43 +0530

    Update Notification.sh

--- Notification.sh
+++ Notification.sh
@@ -1,14 +1,1 @@
-#!/system/bin/sh
-
-# Root required
-if [ "$(id -u)" -ne 0 ]; then
-  echo "Root access required!"
-  exit 1
-fi
-
-# Set title and message dynamically
-title="Task Completed"
-message="The task finished successfully in 5 minutes."
-
-# Post notification with dynamic title and content
-cmd notification post -S bigtext -t "$title" -c "$message" --priority high
+su 2000 -c "cmd notification post -t \"TITLE\" 'Tag' \"CONTENT\""
